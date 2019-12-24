288a05f5e69c9f955cfb15a3ba8846c661e1ef81
Leah Migacz <lmigacz@msudenver.edu>
2019-12-23 22:19:35 -0700

    created an enhanced design

--- script.js
+++ script.js
@@ -7,43 +7,39 @@ function getLocalStorage(key) {
 
 $( document ).ready(function() {
     $("#selectDay").text(moment().format("dddd, MMMM Do"));
+
+// add 9:00 to 5:00 rows to the container
     for (let i = 9; i < 18; i++) {
     
-        // create a row
         var row = $(`<div data-time=${i} id='${i}' class="row">`);
 
-        // create a column
         var col1 = $('<div class="col-sm-2"> <p class="hour">' + timeFormat(i) + '</p>');
 
-        //create column 2
-        var col2 = $(`<div class="col-sm-8 past"><textarea id=text${i} class="description" placeholder="Add your event here..."></textarea>`);        
+        var col2 = $(`<div class="col-sm-8 past"><textarea id=text${i} class="description" placeholder="Add tasks here..."></textarea>`);        
        
-        //create column 3
-        var col3 = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`)
+        var col3 = $(`<div class="col-sm-2"><button class="saveButton" id=${i}><i class="fas fa-save"></i></button>`)
         
-        // append col to row
         row.append(col1);
         row.append(col2);
         row.append(col3);
 
-        // last step add rows to container
         $(".container").append(row);
 
         getLocalStorage(i);
     }
 
+// format hours to correspond to daily hours
     function timeFormat(hours) {
-        var ampm = hours >= 12 ? 'pm' : 'am';
+        var dayTime = hours >= 12 ? ':00 pm' : ':00 am';
         hours = hours % 12;
         hours = hours ? hours : 12;
-        return hours + ampm;
+        return hours + dayTime;
     }
 timeFormat();
 
 function updateColors(){
         var currentTime = new Date().getHours();
         for (var i = 9; i < 18; i++) { 
-        console.log(currentTime, $(`#${i}`).data("time"));
          if ($(`#${i}`).data("time") == currentTime){
             $(`#text${i}`).addClass( "present");
         } else if (currentTime < $(`#${i}`).data("time")) {
@@ -56,8 +52,8 @@ setInterval(function() {
     updateColors();
 }, 1000);
 
-var saveBtn = $('.saveBtn');
-saveBtn.on('click', function(){
+var saveButton = $('.saveButton');
+saveButton.on('click', function(){
     let eventId = $(this).attr('id');
     let eventText = $(this).parent().siblings().children('.description').val();
     localStorage.setItem(eventId, eventText);
